a807521d710bc6d930767462debc19db5d6d8366
Carolina Micherino <101433609+crlnmr@users.noreply.github.com>
2026-09-15 15:47:19 -0300

    Correcao behave IA

--- tests/pages/inventory_page.py
+++ tests/pages/inventory_page.py
@@ -6,9 +6,14 @@ class InventoryPage(BasePage):
 
     ADD_BACKPACK = (By.ID, "add-to-cart-sauce-labs-backpack")
     CART = (By.CLASS_NAME, "shopping_cart_link")
+    INVENTORY_CONTAINER = (By.CLASS_NAME, "inventory_container")
 
     def is_loaded(self):
-        return True
+        try:
+            self.find(*self.INVENTORY_CONTAINER)
+            return True
+        except:
+            return False
 
     def add_product(self):
         self.click(*self.ADD_BACKPACK)
--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPage(BasePage):
-
     USERNAME = (By.ID, "user-name")
     PASSWORD = (By.ID, "password")
     LOGIN_BTN = (By.ID, "login-button")
@@ -18,22 +17,5 @@ class LoginPage(BasePage):
         self.type(*self.PASSWORD, password)
         self.click(*self.LOGIN_BTN)
 
-    def __init__(self, driver):
-        self.driver = driver
-
-    def fill_username(self, username):
-        self.driver.find_element(*self.USERNAME_INPUT).send_keys(username)
-
-    def fill_password(self, password):
-        self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
-
-    def click_login(self):
-        self.driver.find_element(*self.LOGIN_BUTTON).click()
-
-    def login(self, username, password):
-        self.fill_username(username)
-        self.fill_password(password)
-        self.click_login()
-
     def get_error_message(self):
-        return self.driver.find_element(*self.ERROR_MESSAGE).text
+        return self.get_text(*self.ERROR_MESSAGE)
